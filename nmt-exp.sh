f38b0e50d6ae8568861105b0742a51632e27315b
Potsawee Manakul <m.potsawee@gmail.com>
2018-12-05 11:32:49 +0000

    separate model & train code

--- nmt-exp.sh
+++ nmt-exp.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+# souce = load the conda environment that includes
+# python 3.6 bin path
+# TensorFlow (GPU) 1.5.0
 source /home/miproj/urop.2018/pm574/anaconda3/bin/activate tf_gpu
-python model.py
+
+# python 3.6 bin path
+# TensorFlow (CPU) 1.10.0
+# source /home/miproj/urop.2018/pm574/anaconda3/bin/activate tf_cpu
+
+python /home/alta/BLTSpeaking/ged-pm574/local/seq2seq/train.py
